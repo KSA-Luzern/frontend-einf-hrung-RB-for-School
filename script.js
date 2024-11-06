35f6028f6fad41b726a2d6c02d98e5aa8b613b24
RBirr <raphael_birrer2@sluz.ch>
2024-11-06 09:54:52 +0100

    final commit

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
- 
+function go(){
+    let text = document.getElementById("eingabe").value;
+    
+    if(text.includes("hässig")){document.getElementById("gesicht").style.backgroundColor = "red";}
+/*document.getElementById()
+} 
+
 
